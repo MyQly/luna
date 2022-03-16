dc39e4b94f2284d182f10601422bd5be4edda581
Michael Kelley <github@myqly.com>
2022-03-15 20:48:09 -0500

    Added database support for article categories and tags.

--- migrations.lua
+++ migrations.lua
@@ -8,7 +8,7 @@ return {
       { "id", types.serial },
       { "title", types.text },
       { "content", types.text },
-      { "date", types.date({ null = true }) },
+      { "date", types.date },
 
       "PRIMARY KEY (id)"
     })
@@ -28,5 +28,35 @@ return {
   end;
   [3] = function ()
     db.insert("authors", {name = "luna_user", email = "luna_user@luna_blog", password = "luna"})
+  end;
+  [4] = function ()
+    schema.create_table("categories", {
+      { "id", types.serial },
+      { "name", types.text },
+      { "slug", types.text },
+      
+      "PRIMARY KEY (id)"
+    })
+  end;
+  [5] = function ()
+    schema.create_table("tags", {
+      { "id", types.serial },
+      { "name", types.text },
+      { "slug", types.text },
+      
+      "PRIMARY KEY (id)"
+    })
+  end;
+  [6] = function ()
+    schema.add_column("articles", "slug", types.text)
+    schema.add_column("articles", "published", types.boolean)
+    schema.add_column("articles", "category_id", types.foreign_key)
+    db.query("ALTER TABLE articles ADD CONSTRAINT category_fk FOREIGN KEY (category_id) REFERENCES categories (id)")
+  end;
+  [7] = function ()
+    schema.create_table("article_tags", {
+      { "article_id", types.foreign_key },
+      { "tag_id", types.foreign_key }
+    })
   end
 }
