b210d178acbc3b0cb1317fd15fd598f9b5ee927c
Michael Kelley <github@myqly.com>
2022-03-20 16:54:37 -0500

    Added dashboard route. Cleaned up login/logout routes. Added category, tag and slug updates for articles. (Presently in a broken state due to dropping tagify).

--- admin.lua
+++ admin.lua
@@ -1,8 +1,9 @@
 local Authors = require("models.Authors")
 local Articles = require("models.Articles")
 local Categories = require("models.Categories")
--- local Categories = require("models.Categories")
--- local Tags = require("models.Tags")
+local ArticleTags = require("models.ArticleTags")
+local Tags = require("models.Tags")
+
 local md5 = require("md5")
 
 local util = require("lapis.util")
@@ -23,8 +24,14 @@ return function(app)
       local tab = self.params.tab or "articles"
       self.tab = tab
 
+      self.Articles = Articles
+
       if tab == "articles" then
         self.articles = Articles:select()
+      elseif tab == "categories" then
+        self.categories = Categories:select()
+      elseif tab == "tags" then
+        self.tags = Tags:select()
       elseif tab == "authors" then
         self.authors = Authors:select()
       end
@@ -33,27 +40,35 @@ return function(app)
     end
   end
 
-  app:get("/admin", home)
+  app:get("dashboard", "/dashboard", home)
 
   app:get("/admin/:tab", home)
 
-  app:post("/admin/login", function(self)
+  app:get("login", "/login", function(self)
+    return { render = "admin.login", layout = "admin.layout" }
+  end)
+
+  app:post("login", "/login", function(self)
     local password = md5.sumhexa(self.params.password)
     local email = self.params.email
 
     local author = Authors:find({ email = email, password = password})
 
     if not author then
-      return { redirect_to = "/admin?error=true" }
+      return { redirect_to = "/login?error=true" }
     else
       self.session.current_author = { id = author.id, name = author.name, email = author.email }
-      return { redirect_to = "/admin" }
+      return { redirect_to = self:url_for("dashboard") }
     end
   end)
 
-  app:get("/admin/logout", function(self)
+  app:get("/admin", function(self)
+    return {redirect_to = "/dashboard"}
+  end)
+
+  app:get("logout", "/logout", function(self)
     self.session.current_author = nil
-    return { redirect_to = "/admin" }
+    return { redirect_to = "/" }
   end)
 
   app:get("/admin/article/:article", function(self)
@@ -69,9 +84,25 @@ return function(app)
       }
     else
       self.article = Articles:find(self.params.article)
+      self.article_tags = db.query("SELECT name FROM tags WHERE id IN (SELECT tag_id FROM Article_Tags WHERE Article_Id = ?)", self.article.id)
+      self.article_category = db.query("SELECT name FROM categories WHERE id = ?", self.article.category_id)
     end
 
-    self.categories = Categories:select({ fields = "name" })
+    self.tags = Tags:select({fields = "name"})
+    local allTags = ''
+    for k,tag in pairs(self.tags) do
+      allTags = allTags..'"'..tag.name..'", '
+    end
+    allTags = string.sub(allTags, 1, -2)
+    self.allTagList = allTags
+
+    self.categories = Categories:select({ "name" })
+    local allCategories = ''
+    for k,category in pairs(self.categories) do
+      allCategories = allCategories..'"'..category.name..'", '
+    end
+    allCategories = string.sub(allCategories, 1, -2)
+    self.allCategoryList = allCategories
 
     return { render = "admin.article", layout = "admin.layout" }
   end)
@@ -82,7 +113,21 @@ return function(app)
     end
 
     local article
-    local category_id = Categories:find({name = trim(self.params.category)}).id
+    local extractedCategory = ''
+    local category = ''
+
+    if self.params.category == nil then
+      extractedCategory = 'Uncategorized'
+    else
+      extractedCategory = string.sub(self.params.category,12,-4)
+    end
+
+    category = Categories:find({name = extractedCategory})
+
+    if category == nil then
+      local newCategory = db.insert("categories", {name = extractedCategory, slug = slugify(extractedCategory)})
+      category = Categories:find({name = extractedCategory})
+    end
 
     if self.params.article == "new" then
       article = Articles:create({
@@ -91,14 +136,17 @@ return function(app)
         date = db.format_date();
         author_id = self.session.current_author.id;
         slug = slugify(self.params.title);
-        published = true;
-        category_id = category_id;
+        published = self.params.publish;
+        category_id = category.id;
       })
     else
       article = Articles:find(self.params.article)
       article:update({
         title = self.params.title;
         content = self.params.content;
+        category_id = category.id;
+        published = self.params.publish;
+        slug = slugify(self.params.title);
       })
     end
     return { redirect_to = "/admin/article/" .. article.id }
