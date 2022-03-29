6d09645164281824e4493683b9281537b65d2660
Michael Kelley <github@myqly.com>
2022-03-29 00:29:50 -0500

    Reorganized tag & category functionality. Updated article creation to remove references to Tagify.

--- apps/admin/admin.lua
+++ apps/admin/admin.lua
@@ -14,8 +14,80 @@ local db = require("lapis.db")
 local respond_to = require("lapis.application").respond_to
 
 local app_Articles = require("apps.admin.articles")
+local app_Categories = require("apps.admin.categories")
+local app_Tags = require("apps.admin.tags")
 
 return function(app)
+
+  app:get("get_category", "/admin/category/:category", function(self)
+  end)
+
+
+  app:get("get_tag", "/admin/tag/:tag", function(self)
+  end)
+
+  app:get("get_article", "/admin/article/:article", function(self)
+    if not self.session.current_author then
+      return { redirect_to = "/admin" }
+    end
+
+    if self.params.article == "new" then
+      self.article = {
+        id = "new",
+        title = "",
+        content = ""
+      }
+    else
+      self.article = Articles:find(self.params.article)
+      self.article_tags = db.query("SELECT * FROM tags WHERE id IN (SELECT tag_id FROM Article_Tags WHERE Article_Id = ?)", self.article.id)
+      --self.article_category = db.query("SELECT name FROM categories WHERE id = ?", self.article.category_id)
+    end
+
+    self.tags = Tags:select({ "name" })
+    self.categories = Categories:select({ "id", "name" })
+
+    return { render = "admin.article", layout = "admin.layout" }
+  end)
+
+  app:post("update_article", "/admin/article/:article", function(self)
+    if not self.session.current_author then
+      return { status = 401 }
+    end
+
+    local article
+    local category = ''
+
+    category_id = self.params.category or 'Uncategorized'
+
+    category = Categories:find({id = category_id})
+
+    --[[if category == nil then
+      local newCategory = db.insert("categories", {name = category_name, slug = slugify(category_name)})
+      category = Categories:find({name = category_name})
+    end]]--
+
+    if self.params.article == "new" then
+      article = Articles:create({
+        title = self.params.title;
+        content = self.params.content;
+        date = db.format_date();
+        author_id = self.session.current_author.id;
+        slug = slugify(self.params.title);
+        published = self.params.publish;
+        category_id = category.id;
+      })
+    else
+      article = Articles:find(self.params.article)
+      article:update({
+        title = self.params.title;
+        content = self.params.content;
+        category_id = category.id;
+        published = self.params.publish;
+        slug = slugify(self.params.title);
+      })
+    end
+    return { redirect_to = "/admin/article/" .. article.id }
+  end)
 
   app:get("/admin/:tab", function(self)
     return {redirect_to = self:url_for("dashboard")}
@@ -41,6 +113,8 @@ return function(app)
       elseif tab == "authors" then
         self.authors = Authors:select()
         return { render = "admin.manage.authors", layout = "admin.layout" }
+      elseif tab == "depot" then
+        return { render = "admin.manage.depot", layout = "admin.layout" }
       end
 
       return { render = "notfound", layout = "admin.layout" }
@@ -62,88 +136,6 @@ return function(app)
 
   app:get("/admin", function(self)
     return {redirect_to = self:url_for("dashboard")}
-  end)
-
-
-  app:get("/admin/article/:article", function(self)
-    if not self.session.current_author then
-      return { redirect_to = "/admin" }
-    end
-
-    if self.params.article == "new" then
-      self.article = {
-        id = "new",
-        title = "",
-        content = ""
-      }
-    else
-      self.article = Articles:find(self.params.article)
-      self.article_tags = db.query("SELECT name FROM tags WHERE id IN (SELECT tag_id FROM Article_Tags WHERE Article_Id = ?)", self.article.id)
-      self.article_category = db.query("SELECT name FROM categories WHERE id = ?", self.article.category_id)
-    end
-
-    self.tags = Tags:select({fields = "name"})
-    local allTags = ''
-    for k,tag in pairs(self.tags) do
-      allTags = allTags..'"'..tag.name..'", '
-    end
-    allTags = string.sub(allTags, 1, -2)
-    self.allTagList = allTags
-
-    self.categories = Categories:select({ "name" })
-    local allCategories = ''
-    for k,category in pairs(self.categories) do
-      allCategories = allCategories..'"'..category.name..'", '
-    end
-    allCategories = string.sub(allCategories, 1, -2)
-    self.allCategoryList = allCategories
-
-    return { render = "admin.article", layout = "admin.layout" }
-  end)
-
-  app:post("/admin/article/:article", function(self)
-    if not self.session.current_author then
-      return { status = 401 }
-    end
-
-    local article
-    local extractedCategory = ''
-    local category = ''
-
-    if self.params.category == nil then
-      extractedCategory = 'Uncategorized'
-    else
-      extractedCategory = string.sub(self.params.category,12,-4)
-    end
-
-    category = Categories:find({name = extractedCategory})
-
-    if category == nil then
-      local newCategory = db.insert("categories", {name = extractedCategory, slug = slugify(extractedCategory)})
-      category = Categories:find({name = extractedCategory})
-    end
-
-    if self.params.article == "new" then
-      article = Articles:create({
-        title = self.params.title;
-        content = self.params.content;
-        date = db.format_date();
-        author_id = self.session.current_author.id;
-        slug = slugify(self.params.title);
-        published = self.params.publish;
-        category_id = category.id;
-      })
-    else
-      article = Articles:find(self.params.article)
-      article:update({
-        title = self.params.title;
-        content = self.params.content;
-        category_id = category.id;
-        published = self.params.publish;
-        slug = slugify(self.params.title);
-      })
-    end
-    return { redirect_to = "/admin/article/" .. article.id }
   end)
 
 end
